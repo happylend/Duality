36a5ceb07a6c9da8a3104b5579665fc17cb43e09
franciank <xcf85532552@gmail.com>
2022-01-22 16:15:58 +0800

    添加了推拉操作

--- Assets/Scripts/Input/Player_input.cs
+++ Assets/Scripts/Input/Player_input.cs
@@ -7,10 +7,23 @@ public class Player_input
     public float Left { get => Input.GetAxisRaw("Left"); }
     public float Right { get => Input.GetAxisRaw("Right"); }
 
-
+    /// <summary>
+    /// 监测是否按下
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
     public bool GetKeyDown(KeyCode key)
     {
         return Input.GetKeyDown(key);
+    }
+
+    /// <summary>
+    /// 监测是否长按
+    /// </summary>
+    /// <returns></returns>
+    public bool GetKey(KeyCode key)
+    {
+        return Input.GetKey(key);
     }
     // Start is called before the first frame update
 
