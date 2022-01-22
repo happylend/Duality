1bae49ecbab44383d73f23f0a79483e6ba95d1cb
happylend <mengxiangyuhappy@gmail.com>
2022-01-22 21:42:15 +0800

    第一关界面+UI布局

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIController : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
